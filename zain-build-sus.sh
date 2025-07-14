d4b35704e19e56720d6fa508074133f6f4fc64f2
mtkpapa <ebanashka10028@gmail.com>
2025-07-14 13:29:03 +0300

    Add build script

---
+++ zain-build-sus.sh
@@ -0,0 +1,122 @@
+TARGET_DEVICE=$1
+BUILD_DATE=$(date "+%Y%m%d-%H%M")
+
+#ccache
+export CCACHE_DIR="$HOME/.cache/ccache_mikernel"
+export CC="ccache gcc"
+export CXX="ccache g++"
+export PATH="/usr/lib/ccache:$PATH"
+echo "CCACHE_DIR: [$CCACHE_DIR]"
+
+MAKE_ARGS="O=out \
+CC=clang \
+AR=llvm-ar \
+NM=llvm-nm \
+OBJDUMP=llvm-objdump \
+STRIP=llvm-strip \
+HOSTCC=clang \
+HOSTCXX=clang++ \
+LD=ld.lld \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+LLVM=1 \
+LLVM_IAS=1"
+
+if [ ! -f "arch/arm64/configs/${TARGET_DEVICE}_defconfig" ]; then
+    echo "No [${TARGET_DEVICE}] defconfig found."
+    echo "Avaliable defconfigs:"
+    ls arch/arm64/configs/*_defconfig
+    exit 1
+fi
+
+KSU_ZIP_STR=noksu
+if [ "$2" == "ksu" ]; then
+    KSU_E=1
+    KSU_ZIP_STR=ksu
+else
+    KSU_E=0
+fi
+
+#setting up AK
+git clone https://github.com/mtkpapa/AnyKernel3 -b master
+
+if [ $KSU_E -eq 1 ]; then
+    echo "dloading ksu & applying patches"
+    curl -LSs "https://raw.githubusercontent.com/mtkpapa/KernelSU-Next/next-susfs/kernel/setup.sh" | bash -s next-susfs
+#    wget https://gist.githubusercontent.com/zainarbani/2b482e9e9c415a644953397b6ba5571f/raw/b66cf8d0683b5397fc1f7cc6b33ff12cc9bf9292/ksu.patch
+#    git apply ksu.patch
+else 
+    echo "no ksu build"
+fi
+
+rm -rf out/
+
+echo "-OverHeat-LTO-Next" > localversion
+
+#----------------------build shit here
+
+echo "======= START OF BUILD ======="
+make $MAKE_ARGS ${TARGET_DEVICE}_defconfig
+
+if [ $KSU_E -eq 1 ]; then
+    scripts/config --file out/.config -e KSU \
+    -e KSU_SUSFS_HAS_MAGIC_MOUNT \
+    -e KSU_SUSFS_SUS_PATH \
+    -e KSU_SUSFS_SUS_MOUNT \
+    -e KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT \
+    -e KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT \
+    -e KSU_SUSFS_SUS_KSTAT \
+    -e KSU_SUSFS_SUS_OVERLAYFS \
+    -e KSU_SUSFS_TRY_UMOUNT \
+    -e KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT \
+    -e KSU_SUSFS_SPOOF_UNAME \
+    -e KSU_SUSFS_ENABLE_LOG \
+    -e KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS \
+    -e KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
+    -e KSU_SUSFS_OPEN_REDIRECT \
+    -e KSU_SUSFS_SUS_SU
+else
+scripts/config --file out/.config -d KSU \
+    -d KSU_SUSFS_HAS_MAGIC_MOUNT \
+    -d KSU_SUSFS_SUS_PATH \
+    -d KSU_SUSFS_SUS_MOUNT \
+    -d KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT \
+    -d KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT \
+    -d KSU_SUSFS_SUS_KSTAT \
+    -d KSU_SUSFS_SUS_OVERLAYFS \
+    -d KSU_SUSFS_TRY_UMOUNT \
+    -d KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT \
+    -d KSU_SUSFS_SPOOF_UNAME \
+    -d KSU_SUSFS_ENABLE_LOG \
+    -d KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS \
+    -d KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG \
+    -d KSU_SUSFS_OPEN_REDIRECT \
+    -d KSU_SUSFS_SUS_SU
+fi
+
+make $MAKE_ARGS -j$(nproc --all)
+echo "======= END OF BUILD ======="
+
+KOUT_PATH="/mnt/d/users/juan/kernels/${TARGET_DEVICE}/"
+ZIP_NAME="OverHeat-Next-$(date "+%Y%m%d-%H%M").zip"
+
+
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
+    echo "Image found. Build successful"
+    cd AnyKernel3
+	cp ../out/arch/arm64/boot/Image.gz-dtb Image.gz-dtb
+	zip -r9 ../$ZIP_NAME -- *
+	cd ..
+	cp $ZIP_NAME $KOUT_PATH
+	rm -rf $ZIP_NAME
+else
+    echo "Image not found. Pizdec blyat"
+    exit 1
+fi
+
+echo "Cleaning up"
+
+rm -rf KernelSU-Next/
+rm -rf out/
+rm -rf localversion
+rm -rf AnyKernel3
