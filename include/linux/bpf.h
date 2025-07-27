b6f2c0736932eb68873cb5e12d92ef54d7c1da10
mtkpapa <ebanashka10028@gmail.com>
2025-07-27 10:42:11 +0300

    bpf: extend cgroup bpf core to allow multiple cgroup storage types
    https://review.lineageos.org/c/LineageOS/android_kernel_qcom_sm8150/+/434883

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -208,6 +208,12 @@ struct bpf_dev_offload {
 	wait_queue_head_t	verifier_done;
 };
 
+enum bpf_cgroup_storage_type {
+	BPF_CGROUP_STORAGE_SHARED,
+	__BPF_CGROUP_STORAGE_MAX
+};
+#define MAX_BPF_CGROUP_STORAGE_TYPE __BPF_CGROUP_STORAGE_MAX
+
 struct bpf_prog_aux {
 	atomic_t refcnt;
 	u32 used_map_cnt;
@@ -223,7 +229,7 @@ struct bpf_prog_aux {
 #ifdef CONFIG_SECURITY
 	void *security;
 #endif
-	struct bpf_map *cgroup_storage;
+	struct bpf_map *cgroup_storage[MAX_BPF_CGROUP_STORAGE_TYPE];
 	struct bpf_dev_offload *offload;
 	union {
 		struct work_struct work;
@@ -291,7 +297,7 @@ int bpf_prog_test_run_skb(struct bpf_prog *prog, const union bpf_attr *kattr,
  */
 struct bpf_prog_array_item {
 	struct bpf_prog *prog;
-	struct bpf_cgroup_storage *cgroup_storage;
+	struct bpf_cgroup_storage *cgroup_storage[MAX_BPF_CGROUP_STORAGE_TYPE];
 };
 
 struct bpf_prog_array {
